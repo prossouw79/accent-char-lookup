8e3cb3f9b834976a9464e757823735ef85b76e53
Pieter Rossouw <prossouw79@gmail.com>
2022-10-16 10:53:18 +0200

    Remove unnecessary rust code and resize window

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,19 +1,7 @@
-#![cfg_attr(
-	all(not(debug_assertions), target_os = "windows"),
-	windows_subsystem = "windows"
-)]
 use std::process::exit;
-use std::sync::{Arc, Mutex};
-use std::time::Duration;
-use tauri::{Manager, State};
-use tokio::time::sleep;
-
-#[derive(Default)]
-struct Counter(Arc<Mutex<i32>>);
 
 fn main() {
 	tauri::Builder::default()
-		.manage(Counter::default())
 		.invoke_handler(tauri::generate_handler![quit])
 		.run(tauri::generate_context!())
 		.expect("error while running tauri application");
